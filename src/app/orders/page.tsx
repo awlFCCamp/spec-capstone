1069be2cc0cd6ac4e52b58c2f77044da14175a5b
Angela Lee <angelalee@Angelas-iMac.local>
2023-09-27 11:45:58 -0500

    current

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OrdersPage = () => {
   return (
-    <div className="p-4 lg:px-20 xl:px-40">
+    <div className="p-4 lg:px-20 xl:px-40 bg-lime-800 text-white">
       <table className="w-full border-separate border-spacing-3">
         <thead>
           <tr className="text-left">
@@ -14,21 +14,21 @@ const OrdersPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="text-sm md:text-base bg-red-50">
+          <tr className="text-sm md:text-base bg-slate-900">
             <td className="hidden md:block py-6 px-1">1234567890</td>
             <td className="py-6 px-1">01.10.2023</td>
             <td className="py-6 px-1">15.00</td>
             <td className="hidden md:block py-6 px-1">Coffee (1) Bagle(1) </td>
             <td className="py-6 px-1">On the way</td>
           </tr>
-          <tr className="text-sm md:text-base odd:bg-gray-100">
+          <tr className="text-sm md:text-base bg-slate-900">
             <td className="hidden md:block py-6 px-1">1234567890</td>
             <td className="py-6 px-1">01.20.2023</td>
             <td className="py-6 px-1">25.00</td>
             <td className="hidden md:block py-6 px-1">Dozen Cup cakes (1)</td>
             <td className="py-6 px-1">On the way</td>
           </tr>
-          <tr className="text-sm md:text-base odd:bg-gray-100">
+          <tr className="text-sm md:text-base bg-slate-900">
             <td className="hidden md:block py-6 px-1">1234567890</td>
             <td className="py-6 px-1">09.23.23</td>
             <td className="py-6 px-1">26.00</td>
